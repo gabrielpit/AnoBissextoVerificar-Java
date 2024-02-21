1d2b26ba79351613b4a14353e40958f4faa4ac11
gabrielpit <gabrielpit2k19@gmail.com>
2024-02-20 22:00:32 -0300

    commit

---
+++ AnoBisextoVerificar/src/App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o ano inicial");
+        int anoInicial = teclado.nextInt();
+        System.out.println("Digite o ano final");
+        int anoFinal = teclado.nextInt();
+        teclado.close();
+        if(anoInicial < anoFinal){
+        for(int ano = anoInicial; ano <= anoFinal;ano++ ){
+            boolean resultado = eBisexto(ano);
+            System.out.println("Ano "+ano +" e bisexto? " + resultado);
+        }
+    }
+    }
+    static boolean eBisexto(int ano){
+        if(ano % 4 == 0 && ano%100 !=0){
+            return true;
+        }else if(ano % 400 == 0){
+            return true;
+        } else{
+            return false;
+        }
+    }
+}
